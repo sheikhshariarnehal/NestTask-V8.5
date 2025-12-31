035de309315114152f4e96dad45b30e8f062c576
Shariar Nehal <113442689+sheikhshariarnehal@users.noreply.github.com>
2026-01-01 05:31:49 +0600

    app

---
+++ dist/assets/js/browser.DauzW5aD.js
@@ -0,0 +1,1 @@
+import{g as e}from"./charts.Gb_JFp2r.js";const t=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));export{o as b};
